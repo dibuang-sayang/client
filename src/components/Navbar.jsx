67b8eaeeab3e1f5bd7c8c6132579735e80798f01
icanq <icanq@.localdomain>
2021-01-23 02:58:16 +0700

    sentry initialization, checked, turn it off first on development

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 export default function Navbar() {
   return (
-
     <nav class="bg-gray-200">
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
       <div class="relative flex items-center justify-between h-16">
