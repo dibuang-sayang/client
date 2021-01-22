10d64cc85e7c332369ed9fc879d29fe16d8b650e
wypratama <wicaksanapratama@gmail.com>
2021-01-22 21:40:13 +0700

    Add multiple pin to map Add function to popup -> belum bisa dipanggil Remove Warning from class

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Navbar } from './Navbar';
 export { default as Hero } from './Hero';
+export { default as ProductHomepage } from './ProductHomepage';
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,47 @@
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
-import { Hero } from '../components';
+import { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Hero, ProductHomepage } from '../components';
+import dummyData from '../assets/dummy-data-pengepul.json';
+
 export default function Home() {
-  const position = [51.505, -0.09];
+  const [displayPop, setDisplayPop] = useState(null);
+  const position = [-6.260676036065346, 106.78161619719772];
   return (
     <div>
       <Hero />
-      <div class="box-border">
+      {/* Produk Pasar Dummy */}
+      <div className="flex flex-row w-full justify-center">
+        {[...Array(3)].map((el, i) => {
+          return <ProductHomepage key={i} />;
+        })}
+      </div>
+      {/* Map Display */}
+      <div className="box-border w-full flex justify-center my-8">
         <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
           <TileLayer
             className="w-full h-full"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={position}></Marker>
+          {dummyData.map((el, i) => {
+            return (
+              <Marker
+                position={el.lokasi}
+                key={i}
+                onClick={() => {
+                  console.log('tes');
+                }}
+              ></Marker>
+            );
+          })}
+          {displayPop && (
+            <Popup
+              position={displayPop.lokasi}
+              onClose={() => setDisplayPop(null)}
+            >
+              <div>{displayPop.nama}</div>
+            </Popup>
+          )}
         </MapContainer>
       </div>
     </div>
