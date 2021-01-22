5ad47bcc8422c4ae5759c1e43cca36f62f2832d3
wypratama <wicaksanapratama@gmail.com>
2021-01-22 20:15:23 +0700

    Fix Map only display white screen Add Hero to Home Page Move Navbar to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Login, Register, Home } from './pages';
+import { Navbar } from './components';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <div>
         <Switch>
           <Route path="/login" component={Login} />
