ee96a26564606a8c7a87fadd9065f2eb534d6c06
wypratama <wicaksanapratama@gmail.com>
2021-01-25 13:24:18 +0700

    dev-add-cart-popup

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Hero } from './Hero';
 export { default as ProductHomepage } from './ProductHomepage';
 export { default as FooterBar } from './FooterBar';
 export { default as SidebarMarket } from './SidebarMarket';
+export { default as CartPopup } from './CartPopup';
