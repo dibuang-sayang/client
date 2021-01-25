b32544929559f9fa7a4eb7032d43e82298a5bc89
wypratama <wicaksanapratama@gmail.com>
2021-01-25 10:52:52 +0700

    Add cart Page Add cart routing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   AboutUs,
   Contact,
   Market,
+  Cart,
 } from './pages';
 import { Navbar, FooterBar } from './components';
 import { GuardProvider, GuardedRoute } from 'react-router-guards';
@@ -24,6 +25,7 @@ function App() {
             <Route path="/pasar" component={Market} />
             <Route path="/tentang-kami" component={AboutUs} />
             <Route path="/kontak" component={Contact} />
+            <Route path="/keranjang" component={Cart} />
             <GuardedRoute path="/user/:id/setting" component={UserSetting} />
             <Route path="/" exact component={Home} />
           </Switch>
