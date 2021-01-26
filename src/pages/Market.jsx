ca4edf78402ad8014b568781be780cb60530c334
wypratama <wicaksanapratama@gmail.com>
2021-01-26 11:43:10 +0700

    Add Product add pages/Market Add Nested routing to market but fails

--- src/pages/Market.jsx
+++ src/pages/Market.jsx
@@ -1,15 +1,19 @@
-import { FooterBar, SidebarMarket, ProductCard } from '../components';
-import { product } from "../query"
-import { useQuery } from "@apollo/client"
-
+import {
+  FooterBar,
+  SidebarMarket,
+  MarketHome,
+  ProductDetail,
+  ProductAdd,
+} from '../components';
+import { product } from '../query';
+import { useQuery } from '@apollo/client';
+import { Switch, Route } from 'react-router-dom';
 
 export default function Market() {
+  const { loading, error, data: products } = useQuery(product.FIND_ALL_PRODUCT);
 
-  const {loading,error,data:products} = useQuery(product.FIND_ALL_PRODUCT)
-
-
-  if (loading ) return <h1>Loading...</h1>
-  if(error) return <h1>Error !!!</h1>
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <h1>Error !!!</h1>;
   return (
     <section className="text-gray-600 body-font">
       <div className="flex flex-row my-20">
@@ -17,51 +21,13 @@ export default function Market() {
           <SidebarMarket />
         </div>
         <div className="w-9/12 container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap -m-4">
-          {products.products.map(product => {
-            return <ProductCard
-            product = {product}
-            key = {product.id}
-            ></ProductCard>
-          })}
-          </div>
-
-          <div className="flex w-full justify-center mt-10">
-            <div
-              href="#"
-              className="mx-1 px-3 py-2 bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-600 rounded-md cursor-not-allowed"
-            >
-              previous
-            </div>
-
-            <div
-              href="#"
-              className="mx-1 px-3 py-2 bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-200 hover:bg-indigo-600 dark:hover:bg-indigo-500 hover:text-white dark:hover:text-gray-200 rounded-md"
-            >
-              1
-            </div>
-
-            <div
-              href="#"
-              className="mx-1 px-3 py-2 bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-200 hover:bg-indigo-600 dark:hover:bg-indigo-500 hover:text-white dark:hover:text-gray-200 rounded-md"
-            >
-              2
-            </div>
-
-            <div
-              href="#"
-              className="mx-1 px-3 py-2 bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-200 hover:bg-indigo-600 dark:hover:bg-indigo-500 hover:text-white dark:hover:text-gray-200 rounded-md"
-            >
-              3
-            </div>
-
-            <div
-              href="#"
-              className="mx-1 px-3 py-2 bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-200 hover:bg-indigo-600 dark:hover:bg-indigo-500 hover:text-white dark:hover:text-gray-200 rounded-md"
-            >
-              Next
-            </div>
-          </div>
+          <ProductAdd />
+          {/* <Switch>
+            <Route path="/produk/:id" component={ProductDetail} />
+            <Route path="/add" component={ProductAdd} />
+            <Route path="/" component={MarketHome} />
+          </Switch> */}
+          {/* <MarketHome products={products} /> */}
         </div>
       </div>
       <FooterBar />
