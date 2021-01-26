3421ba985bfe992a403eca96cceba5a8eadfa6f3
tiorzal <tioralmunazir@gmail.com>
2021-01-27 01:04:36 +0700

    subhanallah

--- src/App.js
+++ src/App.js
@@ -64,6 +64,7 @@ function App() {
             <Route path="/tentang-kami" component={AboutUs} />
             <Route path="/kontak" component={Contact} />
             <GuardedRoute path="/keranjang" component={Cart} />
+            <GuardedRoute path="/chat/:receiver_id" component={ChatBoard} />
             <GuardedRoute path="/chat" component={ChatBoard} />
             <GuardedRoute path="/user/setting" component={UserSetting} />
             <GuardedRoute path="/office" component={Office}/>
