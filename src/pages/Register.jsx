e2a3cdbab4001840b94f52fd7aaa03ac4abde2f9
tiorzal <tioralmunazir@gmail.com>
2021-01-27 20:51:42 +0700

    remove google button

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -173,10 +173,10 @@ export default function Register() {
               >
                 Register Now
               </button>
-              <button className="flex flex-row gap-3 py-2 justify-center bg-gray-900 w-full font-custom hover:bg-orange-600 text-white px-3 rounded text-lg focus:outline-none shadow">
+              {/* <button className="flex flex-row gap-3 py-2 justify-center bg-gray-900 w-full font-custom hover:bg-orange-600 text-white px-3 rounded text-lg focus:outline-none shadow">
                 <img src={googleLogo} className="w-6 h-6" alt="logo google " />
                 or Sign In with Google
-              </button>
+              </button> */}
               <Link to="/login" className="w-full text-left">
                 <span>
                   Sudah punya akun? <b>Login!</b>
